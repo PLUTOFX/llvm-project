f99aad3f2a2057464f69c37a42450edf754c6fc5
Mircea Trofin <mtrofin@google.com>
2025-12-03 18:48:51 -0800

    [profcheck] Don't verify global ctors

--- llvm/include/llvm/Transforms/Utils/ProfileVerify.h
+++ llvm/include/llvm/Transforms/Utils/ProfileVerify.h
@@ -13,6 +13,7 @@
 #ifndef LLVM_TRANSFORMS_UTILS_PROFILEVERIFY_H
 #define LLVM_TRANSFORMS_UTILS_PROFILEVERIFY_H
 
+#include "llvm/ADT/DenseSet.h"
 #include "llvm/IR/Analysis.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Support/Compiler.h"
@@ -29,8 +30,11 @@ public:
 /// in conjunction with the ProfileInjectorPass. MD_prof "unknown" is considered
 /// valid (i.e. !{!"unknown"})
 class ProfileVerifierPass : public PassInfoMixin<ProfileVerifierPass> {
+  DenseSet<const Function *> IgnoreList;
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM);
+
 public:
-  LLVM_ABI PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM);
+  LLVM_ABI PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM);
 };
 
 } // namespace llvm
